292833d14eb9b7611138082af47251678ffeecd1
Script Raccoon <scriptraccoon@gmail.com>
2025-02-26 08:52:54 +0100

    improve error msg

--- src/env.ts
+++ src/env.ts
@@ -12,11 +12,24 @@ const REDIRECT_URI = process.env.REDIRECT_URI ?? ""
 const REFRESH_TOKEN = process.env.REFRESH_TOKEN ?? ""
 const VIDEO_ID = process.env.VIDEO_ID ?? ""
 
-const is_valid = CLIENT_ID && CLIENT_SECRET && REDIRECT_URI && REFRESH_TOKEN && VIDEO_ID
+if (!CLIENT_ID) {
+	throw new Error("CLIENT_ID is missing.")
+}
 
-if (!is_valid) {
-	console.error("Environment variables are incomplete.")
-	process.exit(1)
+if (!CLIENT_SECRET) {
+	throw new Error("CLIENT_SECRET is missing.")
+}
+
+if (!REDIRECT_URI) {
+	throw new Error("REDIRECT_URI is missing.")
+}
+
+if (!REFRESH_TOKEN) {
+	console.warn("REFRESH_TOKEN is missing.")
+}
+
+if (!VIDEO_ID) {
+	throw new Error("VIDEO_ID is missing.")
 }
 
 export { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, REFRESH_TOKEN, VIDEO_ID }
